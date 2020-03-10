022d3cd4fb29dc5187f086dd0eead15c969957ab
AJ Clemente <areljann.c@gmail.com>
2020-03-09 22:35:23 -0400

    DEVELOP: Connect server and client via socket.io

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -1,7 +1,20 @@
-const Chat = () => (
-  <main>
-    <h1>Chat Page</h1>
-  </main>
-)
+import React, { useState, useEffect } from 'react'
+import socketIOClient from 'socket.io-client'
+
+const Chat = () => {
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:4000/')
+    socket.on('FromAPI', data => setText(data))
+  }, [])
+
+  return (
+    <main>
+      <h1>Chat Page</h1>
+      <p>{text}</p>
+    </main>
+  )
+}
 
 export default Chat
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,15 @@
 const server = require('express')()
+const http = require('http').createServer(server)
+const io = require('socket.io')(http)
 const port = 4000
 
 server.get('/', (req, res) => res.send('hi'))
 
-server.listen(port, () => console.log(`Server running on port ${port}`))
+io.on('connection', socket => {
+  console.log('user connected')
+  socket.emit('FromAPI', 'this emit is from the server')
+})
+
+http.listen(port, () => {
+  console.log(`Listening on port ${port}.`)
+})
