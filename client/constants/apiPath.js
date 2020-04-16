e74795462cd7a77d46e8d46e30a1685ad578b216
AJ Clemente <areljann.c@gmail.com>
2020-04-16 10:09:24 -0400

    CONFIG: Update server API paths for deployment

--- client/constants/apiPath.js
+++ client/constants/apiPath.js
@@ -1,2 +1,2 @@
-export const SERVER_PATH = 'http://localhost:4000'
-export const WEB_SOCKET_PATH = 'ws://localhost:4000'
+export const SERVER_PATH = 'https://chatroom-pwa-server.herokuapp.com'
+export const WEB_SOCKET_PATH = 'wss://chatroom-pwa-server.herokuapp.com'
